3e14f20f6597b610f477a5a38afd3d7e5141f493
niyarrbarman <barmanniyar@gmail.com>
2023-01-26 23:59:48 +0530

    Added success prompt

--- bot.py
+++ bot.py
@@ -4,10 +4,14 @@ from datagen import dataGen
 
 if __name__ == "__main__":
 
+    with open('success.txt') as f:
+        success_prompt = f.read()
+
     process = process()
     try:
         from processed import data
         data_df = dataGen(data).generateTable()
+        print(success_prompt)
     except Exception as e:
         print(e)
     
