3896db93f50b03308ee9dc37345b0ff12a070884
niyarrbarman <barmanniyar@gmail.com>
2023-01-27 21:13:04 +0530

    Automated

--- bot.py
+++ bot.py
@@ -1,8 +1,22 @@
 from processing_data import process
 from datagen import dataGen
+import tweepy
+import keys
 
+def api():
+    auth = tweepy.OAuthHandler(keys.api_key, keys.api_key_secret)
+    auth.set_access_token(keys.access_token, keys.access_token_secret)
+
+    return tweepy.API(auth)
+
+def tweet(api, message, image_path):
+    api.update_status_with_media(message, image_path)
 
 if __name__ == "__main__":
+
+    api = api()
+    message = dataGen.messages()
+    image_path = 'table.png'
 
     with open('success.txt') as f:
         success_prompt = f.read()
@@ -11,6 +25,7 @@ if __name__ == "__main__":
     try:
         from processed import data
         data_df = dataGen(data).generateTable()
+        tweet(api, message, image_path)
         print(success_prompt)
     except Exception as e:
         print(e)
--- datagen.py
+++ datagen.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import dataframe_image as dfi
+import numpy as np
+from datetime import date
 
 class dataGen:
 
@@ -35,6 +37,19 @@ class dataGen:
         df_styled = data_df.style.format({'MMR' : '{:20.2f}'}).hide_index().set_properties(subset=data_df.columns, **{'text-align': 'center'}).set_properties(subset=data_df.columns, **{'width': '90px', 'height': '10px'}).set_table_styles(self.styles).background_gradient(cmap='Blues')
         dfi.export(df_styled, 'table.png', table_conversion='chrome')
         return df_styled
+
+    @staticmethod
+    def messages():
+        today = date.today()
+        today = today.strftime('%B %d, %Y')
+        leaderboard_tweets = [
+            "The ProCity leaderboard is heating up! Check out today's top 10 players and their stats.", 
+            "Who will take the top spot on the ProCity leaderboard today? Stay tuned for the latest updates.", 
+            f"The competition is fierce in ProCity! Here's the latest leaderboard standings for {today}.", 
+            f"Who's leading the pack in ProCity? Check out the latest leaderboard updates for {today}.", 
+            "The ProCity leaderboard is always changing. Who will come out on top today?",]
+        
+        return leaderboard_tweets [np.random.randint(0,len(leaderboard_tweets))]
 
 
 if __name__ == "__main__":
